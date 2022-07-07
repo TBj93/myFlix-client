b18d4ce4ad7b0daf568a5ce556ef4ee240eb3fd0
Tim Burmester <Tim-Burmester1@web.de>
2022-07-07 19:48:17 +0200

    3.6: Installed and enabled routing in react
    Installed yarn react dom routing
    Imported Routing in index.js

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import './main-view.scss';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 import { LoginView } from '../login-view/login-view';
